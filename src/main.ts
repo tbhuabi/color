7f76b3041e5bb550668178a76560b1fa43a568cb
tanbo <464328895@qq.com>
2020-10-30 20:57:53 +0800

    refactor: 理解项目结构

--- src/main.ts
+++
@@ -1,4 +0,0 @@
-import { parseCss } from './lib/index';
-
-const a = parseCss('#fdae99');
-console.log(a);
--- webpack.config.js
+++ webpack.config.js
@@ -1,65 +1,43 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 const path = require('path');
+const ip = require('ip');
 
 module.exports = {
   mode: 'development',
-  devtool: 'cheap-module-eval-source-map',
+  devtool: 'cheap-module-source-map',
   entry: {
-    index: path.resolve(__dirname, 'src/main.ts')
+    index: path.resolve(__dirname, 'public-api.ts')
   },
   output: {
     path: path.resolve(__dirname, 'dist')
   },
   resolve: {
-    extensions: ['.ts', '.js']
+    extensions: ['.ts', '.js'],
+    alias: {
+      '@tanbo/color': path.resolve('src/public-api.ts'),
+      '@tanbo/': path.resolve('src'),
+    }
   },
   devServer: {
-    host: 'localhost',
+    host: ip.address(),
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
-    port: 9200,
+    port: 9006,
     hot: true,
-    open: true
+    open: true,
+    disableHostCheck: true
   },
   module: {
     rules: [{
       test: /\.ts$/,
       loader: ['ts-loader']
-    }, {
-      test: /\.s?css$/,
-      loader: ['style-loader', 'css-loader', {
-        loader: 'postcss-loader',
-        options: {
-          plugins() {
-            return [require('autoprefixer')];
-          }
-        }
-      }, 'sass-loader']
-    }, {
-      test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-      use: [{
-        loader: 'url-loader',
-        options: {
-          limit: 10000,
-          name: path.posix.join('static/', `img/[name][hash].[ext]`)
-        }
-      }],
-    }, {
-      test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-      use: [{
-        loader: 'url-loader',
-        options: {
-          limit: 10000,
-          name: path.posix.join('static/', `fonts/[name][hash].[ext]`)
-        }
-      }],
     }]
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({
-      template: 'src/index.html'
+      template: 'index.html'
     })
   ]
 };
